82466f06939552fdfd35b152482bc53ed66e70ac
raisunny1314 <raisunny1314@gmail.com>
2025-08-25 16:37:07 +0530

    Update index.js

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -18,7 +18,9 @@ app.use(cors({
 }));
 app.use(express.json());
 app.use(cookieParser());
-
+app.get("/", (req, res) => {
+  res.send("🚀 Codex API is running!");
+});
 
 
 app.use("/user", authRouter);
