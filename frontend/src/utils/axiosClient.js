e6a35313c656f117c1e445cf7afc4453e4be0371
raisunny1314 <raisunny1314@gmail.com>
2025-08-25 16:51:10 +0530

    Update axiosClient.js

--- frontend/src/utils/axiosClient.js
+++ frontend/src/utils/axiosClient.js
@@ -1,13 +1,11 @@
-import axios from "axios"
+import axios from "axios";
+
 const axiosClient = axios.create({
-    baseURL: "http://localhost:3000",
-    withCredentials: true,
-    headers: {
-        "Content-Type": "application/json"
-    }
-})
+  baseURL: "https://codex-mulj.onrender.com", // <-- your Render backend URL
+  withCredentials: true,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
 
 export default axiosClient;
-
-
-
